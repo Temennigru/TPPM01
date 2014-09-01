c93da7001a96b393dff9411209685e7ae9c581eb
Temennigru <Jean@Jean-Lucs-MacBook-Pro.local>
2014-09-01 19:31:29 -0300

    Removing unnecessary code

--- Sports/Sports.java
+++ Sports/Sports.java
@@ -1,8 +1,11 @@
 public class Sports {
     private abstract class Sport {
-        protected int[] points = new int[2];
-        this.points[0] = 0;
-        this.points[1] = 0;
+        protected final int[] points = new int[2];
+        
+        public Sport() {
+            this.points[0] = 0;
+            this.points[1] = 0;
+        }
         
         public int victor() {
             if (this.points[1] == this.points[0]) {
@@ -13,20 +16,19 @@ public class Sports {
                 return 0;
             }
         }
-        public void score(int team) { this.score(team, 1); }
         public void score(int team, int score) { this.points[team] += score; }
+        public void score(int team) { this.score(team, 1); }
     }
 
-    public class Basquetball extends Sport {
-        public Basquetball() {}
-    }
+    public class Basquetball extends Sport {}
     
-    public class IndoorVolleyBall extends Sport {
-        public IndoorVolleyBall() {}
-    }
+    public class IndoorVolleyBall extends Sport {}
+    
+    public class IndoorFootball extends Sport {}
+    
+    public class FieldFootball extends Sport {}
 
     public class BeachVolleyball extends Sport {
-        public BeachVolleyball() {}
         public int victor() {
             if (this.points[1] == this.points[0]) {
                 return 2;
@@ -36,14 +38,9 @@ public class Sports {
                 return 1;
             }
         }
-    }
-
-    public class FieldFootball extends Sport {
-        public FieldFootball() {}
     }
 
     public class BeachFootball extends Sport {
-        public BeachFootball() {}
         public int victor() {
             if (this.points[1] == this.points[0]) {
                 return 2;
@@ -55,7 +52,4 @@ public class Sports {
         }
     }
 
-    public class IndoorFootball extends Sport {
-        public IndoorFootball() {}
-    }
 }
