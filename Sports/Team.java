c194a80f7797aed84f8ffec065441a164df803c9
joaopenna <joaopenna@gmail.com>
2014-09-14 16:41:24 -0300

    Create Team.java

---
+++ Sports/Team.java
@@ -0,0 +1,12 @@
+public class Team {
+	    public string countryName;
+	    public int idSport;
+	    public int idTeam;
+	    public int w; //wins
+	    public int d; //draws
+	    public int l; //losses
+	
+	    //conjunto de atletas ?
+	    //match history: vs quem, qual placar ?
+
+}
